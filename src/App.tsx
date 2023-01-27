39e5d639d351a0fe6c255678c952c040ff697ad1
Igor Carvalho de Macedo Antônio <igordemacedo129@gmail.com>
2023-01-27 19:03:49 -0300

    fix: page error 404

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/navbar';
 import { Cart } from './pages/cart/cart';
 //@ts-ignor
